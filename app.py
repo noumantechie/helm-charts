d1fb7d4bd8ef95ced5070ae973f1b62f73769b11
noumantechie <nomanyousaf712@gmail.com>
2025-09-13 02:14:49 +0500

    Updare appversion and version 3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "I am Nouman Yousaf"
+    return "Now kanzal is here!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
